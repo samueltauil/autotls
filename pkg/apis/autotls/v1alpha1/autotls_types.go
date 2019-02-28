c762cd4dff7a871185dfc9a40b5c88f1b8d93cea
Samuel Tauil <samueltauil@gmail.com>
2019-02-28 14:32:55 -0600

    initial commit

---
+++ pkg/apis/autotls/v1alpha1/autotls_types.go
@@ -0,0 +1,49 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// AutoTLSSpec defines the desired state of AutoTLS
+// +k8s:openapi-gen=true
+type AutoTLSSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// AutoTLSStatus defines the observed state of AutoTLS
+// +k8s:openapi-gen=true
+type AutoTLSStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// AutoTLS is the Schema for the autotls API
+// +k8s:openapi-gen=true
+type AutoTLS struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   AutoTLSSpec   `json:"spec,omitempty"`
+	Status AutoTLSStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// AutoTLSList contains a list of AutoTLS
+type AutoTLSList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []AutoTLS `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&AutoTLS{}, &AutoTLSList{})
+}
